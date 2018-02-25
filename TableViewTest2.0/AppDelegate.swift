0147e7f60443fe03b547a3c2231f1b0163385856
Thomas Le Menestrel <TLM@MacBook-Pro-de-Thomas.local>
2018-02-25 15:04:33 +0100

    hello world

--- TableViewTest2.0/AppDelegate.swift
+++ TableViewTest2.0/AppDelegate.swift
@@ -7,62 +7,45 @@
 //
 
 import UIKit
-import Firebase
-import FirebaseMessaging
-import FirebaseInstanceID
-import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate { 
-
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         UITabBar.appearance().tintColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
         UITabBar.appearance().barTintColor = UIColor(red: 151/255, green: 31/255, blue: 35/255, alpha: 1.0)
         UITabBar.appearance().unselectedItemTintColor = UIColor(red: 150/255, green: 150/255, blue: 150/255, alpha: 1.0)
         UINavigationBar.appearance().tintColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
-        FirebaseApp.configure()
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
-            if error == nil {
-             print ("Tu as réussi")
-            }
-            
-        }
-        application.registerForRemoteNotifications()
         
-        NotificationCenter.default.addObserver(self, selector: #selector(self.refreshToken(notification:)), name: NSNotification.Name.InstanceIDTokenRefresh, object: nil)
         return true
     }
-
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        Messaging.messaging().shouldEstablishDirectChannel = false
-        
     }
-
-
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        FBHandler()
     }
     
-    @objc func refreshToken(notification: NSNotification) {
-        let refreshToken = InstanceID.instanceID().token()!
-        print("***\(refreshToken) ***")
-        
-        FBHandler()
-       
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
     
-    func FBHandler () {
-        Messaging.messaging().shouldEstablishDirectChannel = true
-        
-    }
-
-
 }
+
 
